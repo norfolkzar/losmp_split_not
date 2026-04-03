f3c3ef69d7b144366c58c471420fee00834955da
justxyz_h <balakirevegor185@gmail.com>
2026-04-03 19:23:35 +0300

    purely json thing (also screw lounode :wilted_rose:)

--- src/main/java/net/shadow/losmp/screen/EskyBlockScreen.java
+++ src/main/java/net/shadow/losmp/screen/EskyBlockScreen.java
@@ -9,7 +9,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class EskyBlockScreen extends HandledScreen<EskyBlockScreenHandler> {
-    private static final Identifier TEXTURES = new Identifier("textures/gui/");
+    private static final Identifier TEXTURES = new Identifier("minecraft:textures/gui/container/shulker_box.png");
 
     public EskyBlockScreen(EskyBlockScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
